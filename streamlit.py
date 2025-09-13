47767089bade804f813ed3bec4766d1d8f4050ff
shsnge <88963278+shsnge@users.noreply.github.com>
2025-09-13 13:39:40 +0500

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -4,7 +4,7 @@ import streamlit as st
 client = genai.Client(api_key='AIzaSyCM4vIdZylsML_EvYub0ky-ynPuJtYXvUE')
 
 
-st.title("Talk to Agent")
+st.title("Talk to Zeeshan's AI Agent")
 st.write("This app demonstrates a conversational agent.")
 
 user_input = st.text_input("Ask a question:")
@@ -13,4 +13,5 @@ if st.button("Submit"):
         response = client.models.generate_content(
             model='gemini-2.0-flash', contents=user_input
         )
+
     st.write(response.text)
